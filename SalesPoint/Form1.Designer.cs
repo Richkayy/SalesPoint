8c55f9ae518e6dbb3cad4b770bfed5e9561ae9c9
Richkayy <rokrah@yahoo.com>
2013-11-28 12:27:13 -0800

    SalesPoint

---
+++ SalesPoint/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace SalesPoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.cboProduct = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.txtPaid = new System.Windows.Forms.TextBox();
+            this.txtChange = new System.Windows.Forms.TextBox();
+            this.txtDue = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtQty);
+            this.splitContainer1.Panel1.Controls.Add(this.cboProduct);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAddToCart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtPaid);
+            this.splitContainer1.Panel2.Controls.Add(this.txtChange);
+            this.splitContainer1.Panel2.Controls.Add(this.txtDue);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.btnFinish);
+            this.splitContainer1.Size = new System.Drawing.Size(764, 346);
+            this.splitContainer1.SplitterDistance = 496;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(108, 75);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(134, 20);
+            this.txtQty.TabIndex = 9;
+            // 
+            // cboProduct
+            // 
+            this.cboProduct.FormattingEnabled = true;
+            this.cboProduct.Items.AddRange(new object[] {
+            "PAINT",
+            "BRUSH",
+            "COLORS"});
+            this.cboProduct.Location = new System.Drawing.Point(108, 24);
+            this.cboProduct.Name = "cboProduct";
+            this.cboProduct.Size = new System.Drawing.Size(134, 21);
+            this.cboProduct.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PRODUCT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "QUANTITY";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 184);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(416, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Location = new System.Drawing.Point(108, 127);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(124, 38);
+            this.btnAddToCart.TabIndex = 0;
+            this.btnAddToCart.Text = "ADD TO CART";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // txtPaid
+            // 
+            this.txtPaid.Location = new System.Drawing.Point(118, 72);
+            this.txtPaid.Name = "txtPaid";
+            this.txtPaid.Size = new System.Drawing.Size(134, 20);
+            this.txtPaid.TabIndex = 8;
+            // 
+            // txtChange
+            // 
+            this.txtChange.Location = new System.Drawing.Point(118, 124);
+            this.txtChange.Name = "txtChange";
+            this.txtChange.Size = new System.Drawing.Size(134, 20);
+            this.txtChange.TabIndex = 8;
+            // 
+            // txtDue
+            // 
+            this.txtDue.Location = new System.Drawing.Point(118, 25);
+            this.txtDue.Name = "txtDue";
+            this.txtDue.Size = new System.Drawing.Size(134, 20);
+            this.txtDue.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "CHANGE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "AMOUNT PAID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TOTAL COST";
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Location = new System.Drawing.Point(77, 195);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(123, 44);
+            this.btnFinish.TabIndex = 0;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click_1);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Product";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Quantity";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Unit Cost";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Total Cost";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 346);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.ComboBox cboProduct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPaid;
+        private System.Windows.Forms.TextBox txtChange;
+        private System.Windows.Forms.TextBox txtDue;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ SalesPoint/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SalesPoint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        decimal total = 0;
+
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            int qty = 0;
+            if (cboProduct.SelectedIndex == -1)
+            {
+                MessageBox.Show("PLEASE SELECT A PRODUCT!");
+                cboProduct.Focus();
+                return;
+            }
+            //
+            if (!string.IsNullOrEmpty(txtQty.Text))
+            {
+                try
+                {
+
+                    qty = int.Parse(txtQty.Text);
+                    total = total + decimal.Parse((2.5 * qty) + "");
+                    txtDue.Text = total.ToString();
+                }
+                catch (Exception eex)
+                {
+
+                    MessageBox.Show("Invalid quantity",
+                        this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Console.WriteLine(eex.Message + " occurred in btnAddtoCart");
+                    return;
+                }
+
+            }
+            else if (string.IsNullOrEmpty(txtQty.Text))
+            {
+                MessageBox.Show("Please specify qty!");
+                txtQty.Focus();
+                return;
+            }
+
+            string item = cboProduct.Text;
+            decimal price = 2.5m;
+
+            decimal ExtPrice = price * qty;
+
+            string[] cartline = {
+                                     item, 
+                                     price.ToString("F2"),
+                                     qty.ToString(),
+                                     ExtPrice.ToString("F2")
+                                 };
+
+            dataGridView1.Rows.Add(cartline);
+
+            ClearEntry();
+
+
+        }
+
+        private void ClearEntry()
+        {
+            cboProduct.SelectedIndex = -1;
+            txtQty.Text = "";
+        }
+
+        private void txtDue_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int qty = int.Parse(txtQty.Text);
+                string item = cboProduct.Text;
+                decimal price = 2.5m;
+                decimal ExtPrice = price * qty;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("INCORRECT ENTRY");
+            }
+        }
+
+
+
+
+
+        private void btnFinish_Click_1(object sender, EventArgs e)
+        {
+
+
+            decimal paid = decimal.Parse((txtPaid.Text) + "");
+            if (paid >= total)
+            {
+
+                decimal change = paid - total;
+                txtChange.Text = change.ToString();
+                MessageBox.Show("TRANSACTION COMPLETED");
+            }
+            else
+            {
+
+                MessageBox.Show("PAYMENT IS LOWER THAN TOTAL AMOUNT");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+       
+      
+
+
+    }
+}
